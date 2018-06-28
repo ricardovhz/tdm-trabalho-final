e5c2b3a9994917c89288cea95964d1c60d272df4
Ricardo Vanhoz Ribeiro <ricardo.vanhoz@gmail.com>
2018-06-28 15:33:13 -0300

    diretorios

--- web/task/src/environments/environment.prod.ts
+++ web/task/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBtz7_XXnYvpEdHt7QOXDc1dVCBy4_w9lE",
-    authDomain: "tdmtrabalhofinal.firebaseapp.com",
-    databaseURL: "https://tdmtrabalhofinal.firebaseio.com",
-    projectId: "tdmtrabalhofinal",
-    storageBucket: "tdmtrabalhofinal.appspot.com",
-    messagingSenderId: "448043918113"
+    apiKey: "", // your API key
+    authDomain: "", 
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
